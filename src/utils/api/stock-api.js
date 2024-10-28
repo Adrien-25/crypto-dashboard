c9feb2544c5e9673550ef35278cc5d3b497ccd2a
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2024-10-28 12:25:36 +0100

    add comonents contents

--- src/utils/api/stock-api.js
+++ src/utils/api/stock-api.js
@@ -1,35 +1,77 @@
+const basePath = "https://finnhub.io/api/v1";
+
 /**
- * Converts a Date Object to a UNIX timestamp
- * @param {Date} date - The Date Object to be converted
- * @param {number} unixTimestamp The corresponding UNIX timestamp (seconds elapsed since January 1st, 1970 at UTC)
+ * Searches best stock matches based on a user's query
+ * @param {string} query - The user's query, e.g. 'fb'
+ * @returns {Promise<Object[]>} Response array of best stock matches
  */
-export const convertDateToUnixTimestamp = (date) => {
-    return Math.floor(date.getTime() / 1000);
-  };
-  
-  /**
-   * Converts a UNIX timestamp to a Date
-   * @param {number} unixTimestamp - UNIX timestamp (seconds elapsed since January 1st, 1970 at UTC)
-   * @returns {string} The corresponding Date Object formatted as a string
-   */
-  export const convertUnixTimestampToDate = (unixTimestamp) => {
-    const milliseconds = unixTimestamp * 1000;
-    return new Date(milliseconds).toLocaleDateString();
-  };
-  
-  /**
-   * Creates a new date by adding days/weeks/months/years to a given date. Negative values will also work (for past dates)
-   * @param {Date} date - The specified date
-   * @param {number} days - The number (integer) of days to be added/subtracted
-   * @param {number} weeks - The number (integer) of weeks to be added/subtracted
-   * @param {number} months - The number (integer) of months to be added/subtracted
-   * @param {number} years - The number (integer) of years to be added/subtracted
-   * @returns {Date} The new date
-   */
-  export const createDate = (date, days, weeks, months, years) => {
-    let newDate = new Date(date);
-    newDate.setDate(newDate.getDate() + days + 7 * weeks);
-    newDate.setMonth(newDate.getMonth() + months);
-    newDate.setFullYear(newDate.getFullYear() + years);
-    return newDate;
-  };
+export const searchSymbol = async (query) => {
+  const url = `${basePath}/search?q=${query}&token=${process.env.REACT_APP_API_KEY}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  return await response.json();
+};
+
+/**
+ * Fetches the details of a given company
+ * @param {string} stockSymbol - Symbol of the company, e.g. 'FB'
+ * @returns {Promise<Object>} Response object
+ */
+export const fetchStockDetails = async (stockSymbol) => {
+  const url = `${basePath}/stock/profile2?symbol=${stockSymbol}&token=${process.env.REACT_APP_API_KEY}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  return await response.json();
+};
+
+/**
+ * Fetches the latest quote of a given stock
+ * @param {string} stockSymbol - Symbol of the company, e.g. 'FB'
+ * @returns {Promise<Object>} Response object
+ */
+export const fetchQuote = async (stockSymbol) => {
+  const url = `${basePath}/quote?symbol=${stockSymbol}&token=${process.env.REACT_APP_API_KEY}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  return await response.json();
+};
+
+/**
+ * Fetches historical data of a stock (to be displayed on a chart)
+ * @param {string} stockSymbol - Symbol of the company, e.g. 'FB'
+ * @param {string} resolution - Resolution of timestamps. Supported resolution includes: 1, 5, 15, 30, 60, D, W, M
+ * @param {number} from - UNIX timestamp (seconds elapsed since January 1st, 1970 at UTC). Interval initial value.
+ * @param {number} to - UNIX timestamp (seconds elapsed since January 1st, 1970 at UTC). Interval end value.
+ * @returns {Promise<Object>} Response object
+ */
+export const fetchHistoricalData = async (
+  stockSymbol,
+  resolution,
+  from,
+  to
+) => {
+  const url = `${basePath}/stock/candle?symbol=${stockSymbol}&resolution=${resolution}&from=${from}&to=${to}&token=${process.env.REACT_APP_API_KEY}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  return await response.json();
+};
