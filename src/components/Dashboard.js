ac1ff88a13d65100c7e2cf31352145e451532c69
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2024-10-28 11:41:58 +0100

    starting project

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div
+    className={`h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand"
+    }`}
+  >
+    
+  </div>
+  );
+};
+export default Dashboard;
