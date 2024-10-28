47354afc462ae15d9589e7b04d6d0c08a492a7cf
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2024-10-28 11:59:05 +0100

    add context in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 import Dashboard from "./components/Dashboard";
+import StockContext from "./context/StockContext";
+import ThemeContext from "./context/ThemeContext";
+
 function App() {
-  return <Dashboard />;
+  const [darkMode, setDarkMode] = useState(false);
+  const [stockSymbol, setStockSymbol] = useState("MSFT");
+  return (
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      <StockContext.Provider value={{ stockSymbol, setStockSymbol }}>
+        <Dashboard />
+      </StockContext.Provider>
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
+
+// import { useState } from "react";
+// import "./App.css";
+// import Dashboard from "./components/Dashboard";
+// import StockContext from "./context/StockContext";
+// import ThemeContext from "./context/ThemeContext";
+
+// function App() {
+//   const [darkMode, setDarkMode] = useState(false);
+//   const [stockSymbol, setStockSymbol] = useState("MSFT");
+
+//   return (
+//     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+//       <StockContext.Provider value={{ stockSymbol, setStockSymbol }}>
+//         <Dashboard />
+//       </StockContext.Provider>
+//     </ThemeContext.Provider>
+//   );
+// }
+
+// export default App;
