6bce8ca896621bc6eb1c1392ce5fcf0c29f58dcf
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2024-10-28 22:30:12 +0100

    heroicon v1 and minor corrections

--- src/App.js
+++ src/App.js
@@ -18,23 +18,4 @@ function App() {
 
 export default App;
 
-// import { useState } from "react";
-// import "./App.css";
-// import Dashboard from "./components/Dashboard";
-// import StockContext from "./context/StockContext";
-// import ThemeContext from "./context/ThemeContext";
 
-// function App() {
-//   const [darkMode, setDarkMode] = useState(false);
-//   const [stockSymbol, setStockSymbol] = useState("MSFT");
-
-//   return (
-//     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
-//       <StockContext.Provider value={{ stockSymbol, setStockSymbol }}>
-//         <Dashboard />
-//       </StockContext.Provider>
-//     </ThemeContext.Provider>
-//   );
-// }
-
-// export default App;
